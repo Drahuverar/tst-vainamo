df8f79d7d29c7c016fedae06454dce9c28835354
Drahuverar <drahuverar@gmail.com>
2016-12-01 18:09:23 +0100

    Ich denke das sollte so klappen.

---
+++ Test/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DockPanelSkin1 As WeifenLuo.WinFormsUI.Docking.DockPanelSkin = New WeifenLuo.WinFormsUI.Docking.DockPanelSkin()
+        Dim AutoHideStripSkin1 As WeifenLuo.WinFormsUI.Docking.AutoHideStripSkin = New WeifenLuo.WinFormsUI.Docking.AutoHideStripSkin()
+        Dim DockPanelGradient1 As WeifenLuo.WinFormsUI.Docking.DockPanelGradient = New WeifenLuo.WinFormsUI.Docking.DockPanelGradient()
+        Dim TabGradient1 As WeifenLuo.WinFormsUI.Docking.TabGradient = New WeifenLuo.WinFormsUI.Docking.TabGradient()
+        Dim DockPaneStripSkin1 As WeifenLuo.WinFormsUI.Docking.DockPaneStripSkin = New WeifenLuo.WinFormsUI.Docking.DockPaneStripSkin()
+        Dim DockPaneStripGradient1 As WeifenLuo.WinFormsUI.Docking.DockPaneStripGradient = New WeifenLuo.WinFormsUI.Docking.DockPaneStripGradient()
+        Dim TabGradient2 As WeifenLuo.WinFormsUI.Docking.TabGradient = New WeifenLuo.WinFormsUI.Docking.TabGradient()
+        Dim DockPanelGradient2 As WeifenLuo.WinFormsUI.Docking.DockPanelGradient = New WeifenLuo.WinFormsUI.Docking.DockPanelGradient()
+        Dim TabGradient3 As WeifenLuo.WinFormsUI.Docking.TabGradient = New WeifenLuo.WinFormsUI.Docking.TabGradient()
+        Dim DockPaneStripToolWindowGradient1 As WeifenLuo.WinFormsUI.Docking.DockPaneStripToolWindowGradient = New WeifenLuo.WinFormsUI.Docking.DockPaneStripToolWindowGradient()
+        Dim TabGradient4 As WeifenLuo.WinFormsUI.Docking.TabGradient = New WeifenLuo.WinFormsUI.Docking.TabGradient()
+        Dim TabGradient5 As WeifenLuo.WinFormsUI.Docking.TabGradient = New WeifenLuo.WinFormsUI.Docking.TabGradient()
+        Dim DockPanelGradient3 As WeifenLuo.WinFormsUI.Docking.DockPanelGradient = New WeifenLuo.WinFormsUI.Docking.DockPanelGradient()
+        Dim TabGradient6 As WeifenLuo.WinFormsUI.Docking.TabGradient = New WeifenLuo.WinFormsUI.Docking.TabGradient()
+        Dim TabGradient7 As WeifenLuo.WinFormsUI.Docking.TabGradient = New WeifenLuo.WinFormsUI.Docking.TabGradient()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.JobBox = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.DockPanel1 = New WeifenLuo.WinFormsUI.Docking.DockPanel()
+        CType(Me.JobBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(147, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(137, 120)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 3
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(69, 177)
+        Me.Panel1.TabIndex = 4
+        '
+        'JobBox
+        '
+        Me.JobBox.Image = Global.Test.My.Resources.Resources.DaTweekaz___Dragonduck___Kopie
+        Me.JobBox.Location = New System.Drawing.Point(41, 200)
+        Me.JobBox.Name = "JobBox"
+        Me.JobBox.Size = New System.Drawing.Size(50, 50)
+        Me.JobBox.TabIndex = 0
+        Me.JobBox.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(147, 163)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(100, 87)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Test.My.Resources.Resources.Image_1
+        Me.PictureBox1.Location = New System.Drawing.Point(147, 38)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'DockPanel1
+        '
+        Me.DockPanel1.ActiveAutoHideContent = Nothing
+        Me.DockPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.DockPanel1.DockBackColor = System.Drawing.SystemColors.ControlLight
+        Me.DockPanel1.Location = New System.Drawing.Point(634, 0)
+        Me.DockPanel1.Name = "DockPanel1"
+        Me.DockPanel1.Size = New System.Drawing.Size(332, 568)
+        DockPanelGradient1.EndColor = System.Drawing.SystemColors.ControlLight
+        DockPanelGradient1.StartColor = System.Drawing.SystemColors.ControlLight
+        AutoHideStripSkin1.DockStripGradient = DockPanelGradient1
+        TabGradient1.EndColor = System.Drawing.SystemColors.Control
+        TabGradient1.StartColor = System.Drawing.SystemColors.Control
+        TabGradient1.TextColor = System.Drawing.SystemColors.ControlDarkDark
+        AutoHideStripSkin1.TabGradient = TabGradient1
+        DockPanelSkin1.AutoHideStripSkin = AutoHideStripSkin1
+        TabGradient2.EndColor = System.Drawing.SystemColors.ControlLightLight
+        TabGradient2.StartColor = System.Drawing.SystemColors.ControlLightLight
+        TabGradient2.TextColor = System.Drawing.SystemColors.ControlText
+        DockPaneStripGradient1.ActiveTabGradient = TabGradient2
+        DockPanelGradient2.EndColor = System.Drawing.SystemColors.Control
+        DockPanelGradient2.StartColor = System.Drawing.SystemColors.Control
+        DockPaneStripGradient1.DockStripGradient = DockPanelGradient2
+        TabGradient3.EndColor = System.Drawing.SystemColors.ControlLight
+        TabGradient3.StartColor = System.Drawing.SystemColors.ControlLight
+        TabGradient3.TextColor = System.Drawing.SystemColors.ControlText
+        DockPaneStripGradient1.InactiveTabGradient = TabGradient3
+        DockPaneStripSkin1.DocumentGradient = DockPaneStripGradient1
+        TabGradient4.EndColor = System.Drawing.SystemColors.ActiveCaption
+        TabGradient4.LinearGradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical
+        TabGradient4.StartColor = System.Drawing.SystemColors.GradientActiveCaption
+        TabGradient4.TextColor = System.Drawing.SystemColors.ActiveCaptionText
+        DockPaneStripToolWindowGradient1.ActiveCaptionGradient = TabGradient4
+        TabGradient5.EndColor = System.Drawing.SystemColors.Control
+        TabGradient5.StartColor = System.Drawing.SystemColors.Control
+        TabGradient5.TextColor = System.Drawing.SystemColors.ControlText
+        DockPaneStripToolWindowGradient1.ActiveTabGradient = TabGradient5
+        DockPanelGradient3.EndColor = System.Drawing.SystemColors.ControlLight
+        DockPanelGradient3.StartColor = System.Drawing.SystemColors.ControlLight
+        DockPaneStripToolWindowGradient1.DockStripGradient = DockPanelGradient3
+        TabGradient6.EndColor = System.Drawing.SystemColors.GradientInactiveCaption
+        TabGradient6.LinearGradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical
+        TabGradient6.StartColor = System.Drawing.SystemColors.GradientInactiveCaption
+        TabGradient6.TextColor = System.Drawing.SystemColors.ControlText
+        DockPaneStripToolWindowGradient1.InactiveCaptionGradient = TabGradient6
+        TabGradient7.EndColor = System.Drawing.Color.Transparent
+        TabGradient7.StartColor = System.Drawing.Color.Transparent
+        TabGradient7.TextColor = System.Drawing.SystemColors.ControlDarkDark
+        DockPaneStripToolWindowGradient1.InactiveTabGradient = TabGradient7
+        DockPaneStripSkin1.ToolWindowGradient = DockPaneStripToolWindowGradient1
+        DockPanelSkin1.DockPaneStripSkin = DockPaneStripSkin1
+        Me.DockPanel1.Skin = DockPanelSkin1
+        Me.DockPanel1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(966, 568)
+        Me.Controls.Add(Me.DockPanel1)
+        Me.Controls.Add(Me.JobBox)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.JobBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents JobBox As PictureBox
+    Friend WithEvents DockPanel1 As WeifenLuo.WinFormsUI.Docking.DockPanel
+End Class
---
+++ Test/Form1.vb
@@ -0,0 +1,75 @@
+﻿Public Class Form1
+
+    ' One control named "JobBox" (I used a picbox), multiple panels named anything you want. 
+
+    Dim isDrag As Boolean = False
+    Dim lastparent As String
+    Dim lastlocation As Point
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ' move JobBox into one of our panels
+        ' JobBox.Parent = Panel1
+        ' JobBox.Location = New Point(10, 10)
+    End Sub
+
+    ' Start Drag
+    Private Sub JobBox_MouseDown(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles JobBox.MouseDown
+        If (e.Button = MouseButtons.Left) Then
+            isDrag = True
+            Me.Cursor = Cursors.Hand
+            ' rem current parent & location incase we need to move control back.
+            lastparent = JobBox.Parent.Name
+            lastlocation = JobBox.Location
+            ' move control to form/front so we can drag it around.
+            JobBox.Parent = Me
+            JobBox.BringToFront()
+        End If
+    End Sub
+
+    ' dragging
+    Private Sub JobBox_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles JobBox.MouseMove
+        If isDrag = True Then
+            JobBox.Left = (JobBox.Left + e.X)
+            JobBox.Top = (JobBox.Top + e.Y)
+        End If
+    End Sub
+
+    ' dropped
+    Private Sub JobBox_MouseUp(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles JobBox.MouseUp
+        isDrag = False
+        Me.Cursor = Cursors.Default
+        Dim i As Integer
+        Dim foundNewHome As Boolean = False
+        Dim controlRectangle As Rectangle
+        ' where are we now?
+        Dim startPoint As Point = JobBox.PointToScreen(New Point(e.X, e.Y))
+        Dim theRectangle As Rectangle = New Rectangle(startPoint.X, startPoint.Y, 0, 0)
+        ' what control did we get dropped on?
+        For i = 0 To Controls.Count - 1
+            ' Allow drop in panels only!
+            If TypeOf Controls(i) Is Panel Then ' note: 1-layer, doesn't support panels inside panels!
+                controlRectangle = Controls(i).RectangleToScreen(Controls(i).ClientRectangle)
+                If controlRectangle.IntersectsWith(theRectangle) Then
+                    ' get drop location of the new container
+                    Dim containerLocation As Point = PointToScreen(Controls(i).Location)
+                    Dim dropTarget As Point = New Point(Cursor.Position.X - containerLocation.X, Cursor.Position.Y - containerLocation.Y)
+                    ' move control into new container at dropped location.
+                    JobBox.Parent = Controls(i)
+                    JobBox.Location = dropTarget
+                    JobBox.BringToFront()
+                    foundNewHome = True
+                    Exit For
+                End If
+            End If
+        Next
+        If foundNewHome = False Then
+            ' control was dropped on form or non-panel, so move back to original container.
+            'MessageBox.Show("Can't drop here!")
+            JobBox.Parent = Me.Controls(lastparent)
+            JobBox.Location = lastlocation
+            JobBox.BringToFront()
+        End If
+    End Sub
+
+
+End Class
